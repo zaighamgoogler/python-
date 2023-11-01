aef2ffa8525cd1b173868a233fa2632bf4cf5046
zaighamgoogler <110395678+zaighamgoogler@users.noreply.github.com>
2023-11-01 22:44:43 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+operation = str(input("what operation do you want to do:"))
+a = int(input("enter the first number:"))
+b = int(input("enter the second number:"))
+if (operation == "addition"):
+  print(a + b)
+elif (operation == "subtraction"):
+  print(a - b)
+
+elif (operation == "multiplication"):
+  print(a * b)
+
+elif (operation == "division"):
+  print(a / b)
